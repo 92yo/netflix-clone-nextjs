4f7ea4c1f9dd3b9da1662327c83925c95adc0dc3
92yo <yousefrizik92@gmail.com>
2023-03-26 13:14:41 +0300

    adds movieList, movieCard and movies endpoint func

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import { NextPageContext } from "next";
-import { getSession, signOut } from "next-auth/react";
+import { getSession } from "next-auth/react";
+
 import Navbar from "@/components/Navbar";
 import BillBoard from "@/components/BillBoard";
+import MovieList from "@/components/MovieList";
 
-import useCurrentUser from "@/hooks/useCurrentUser";
+import useMovieList from "@/hooks/useMovieList";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -23,12 +25,15 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
+  const { data: movies = [] } = useMovieList();
 
   return (
     <>
       <Navbar />
       <BillBoard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+      </div>
     </>
   );
 }
